0ee06e7a71cc3ce7363e1a158ee0f3991a60c314
Инара <inarashugaikina@gmail.com>
2020-12-29 20:31:59 +0600

    Add Group models.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Post
-
 # Register your models here.
 
 
@@ -12,3 +11,7 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+
+
+
+
