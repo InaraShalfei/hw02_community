f021608a7b76813d57f2435720986aa5b66f57ef
Инара <inarashugaikina@gmail.com>
2020-12-30 19:46:09 +0600

    Final commit

--- posts/models.py
+++ posts/models.py
@@ -3,14 +3,15 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 User = get_user_model()
-Group = get_user_model()
 
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey("Group", on_delete=models.CASCADE, related_name="group", blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
+    group = models.ForeignKey("Group", on_delete=models.CASCADE,
+                              related_name="group", blank=True, null=True)
 
 
 class Group(models.Model):
