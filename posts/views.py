b34f55cb7c349d07258c2e5c551a04546e5ee436
Инара <inarashugaikina@gmail.com>
2021-01-04 13:53:32 +0600

    Syntaxis changes

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post, Group
 
 
-# Create your views here.
 def index(request):
     latest = Post.objects.order_by('-pub_date')[:11]
     return render(request, "index.html", {"posts": latest})
