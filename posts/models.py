b1d30b0979ad73861714cf710aceff137e9e0958
Инара <inarashugaikina@gmail.com>
2021-01-09 12:25:51 +0600

    Alter verbose names

--- posts/models.py
+++ posts/models.py
@@ -1,35 +1,35 @@
 from django.db import models
-
 from django.contrib.auth import get_user_model
 
-from django.db.models.options import Options
 
 User = get_user_model()
 
 
 class Post(models.Model):
-    Options.verbose_name = "post"
-    Options.verbose_name_plural = "posts"
-    text = models.TextField("post's text")
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    text = models.TextField("Текст поста")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="posts",
-                               verbose_name="post's author")
+                               verbose_name="Автор поста")
     group = models.ForeignKey("Group", on_delete=models.SET_NULL,
                               related_name="posts",
                               blank=True,
-                              verbose_name="post's group", null=True)
+                              verbose_name="Группа поста", null=True)
 
     class Meta:
         ordering = ['-pub_date']
+        verbose_name = "post"
+        verbose_name_plural = "posts"
 
 
 class Group(models.Model):
-    Options.verbose_name = "group"
-    Options.verbose_name_plural = "groups"
-    title = models.CharField("group's title", max_length=200)
-    slug = models.SlugField("group's url", unique=True)
-    description = models.TextField("group's details")
+    title = models.CharField("Название группы", max_length=200)
+    slug = models.SlugField("Адрес группы", unique=True)
+    description = models.TextField("Описание группы")
+
+    class Meta:
+        verbose_name = "group"
+        verbose_name_plural = "groups"
 
     def __str__(self):
         return self.title
